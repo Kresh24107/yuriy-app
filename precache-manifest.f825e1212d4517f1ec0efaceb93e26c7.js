6180ee23adc98b280e26b619cb47e2d772a9f70f
Vadim Sirenko <vadim7ko@gmail.com>
2019-09-06 18:54:28 +0300

    Updates

---
+++ precache-manifest.f825e1212d4517f1ec0efaceb93e26c7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe359e109568a3a82144e69bb5cbcf63",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "c8a59be98ddaefe92ac5",
+    "url": "/myapp/static/js/2.7898fcca.chunk.js"
+  },
+  {
+    "revision": "6c5c52963e97f6bbdb49",
+    "url": "/myapp/static/js/main.d4e6afae.chunk.js"
+  },
+  {
+    "revision": "f68efd9f3e5a7883bfff",
+    "url": "/myapp/static/js/runtime~main.b1cf388b.js"
+  }
+]);
